ce8e6e851b2fdd17db7b8ba87c1816ec2abe18d2
Tomasz Polański <polanski.tomek@gmail.com>
2016-04-01 08:29:30 +0200

    Added NAudio

---
+++ MarkovCritic/Test.fsx
@@ -0,0 +1,11 @@
+﻿#r "../packages/NAudio.1.7.3/lib/net35/NAudio.dll"
+
+open NAudio;
+open NAudio.Wave;
+
+let test (mp3File: string) = 
+    use file1 = new Mp3FileReader(mp3File)
+    printf "%A" file1
+    WaveFileWriter.CreateWaveFile(@"C:\Users\Tomasz\OneDrive\Music\test.wav", file1);
+    
+test @"C:\Users\Tomasz\OneDrive\Music\moby flower.mp3" 
