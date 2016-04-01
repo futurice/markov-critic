a60ab6b5650847e27981c3a2a225485706d6aec3
pingzing <neil.mcalister@futurice.com>
2016-04-01 16:40:00 +0300

    Finish fixing git brokenness

---
+++ MarkovCritic/CorpusScraper.fsx
@@ -0,0 +1,5 @@
+﻿#r "../packages/FSharp.Data.2.2.5/lib/net40/FSharp.Data.dll"
+
+open FSharp.Data
+
+let results = FSharp.Data.HtmlDocument.Load("http://www.rottentomatoes.com/browse/dvd-all/?maxTomato=50&services=amazon;amazon_prime;flixster;hbo_go;itunes;netflix_iw;vudu#")
